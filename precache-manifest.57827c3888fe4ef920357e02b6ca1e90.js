31a713d5d89ef17f420411b7467f7f9559ea67b4
korneevskii <korneevskii@gmail.com>
2020-06-05 19:31:26 +0300

    Updates

---
+++ precache-manifest.57827c3888fe4ef920357e02b6ca1e90.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9d4213c5171b61486c4371f5512532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8039d2d2fc55b002aa",
+    "url": "/static/css/main.f2bf2fc9.chunk.css"
+  },
+  {
+    "revision": "2ea4e228dfd771731563",
+    "url": "/static/js/2.94ee509a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ee509a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8039d2d2fc55b002aa",
+    "url": "/static/js/main.474f5c8a.chunk.js"
+  },
+  {
+    "revision": "bd20bd05229373a1c10b",
+    "url": "/static/js/runtime-main.ad630a95.js"
+  },
+  {
+    "revision": "1e6ae1eead98df0dadce3c7f6b10aefc",
+    "url": "/static/media/geometria-bold.1e6ae1ee.ttf"
+  },
+  {
+    "revision": "45b00bb5d403795df29be8874267b45a",
+    "url": "/static/media/geometria-bold.45b00bb5.woff2"
+  },
+  {
+    "revision": "472a653cd97c617986d64b7071794eac",
+    "url": "/static/media/geometria-bold.472a653c.eot"
+  },
+  {
+    "revision": "48e7ec72bf504d84ac56923487bf1bb9",
+    "url": "/static/media/geometria-bold.48e7ec72.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-bold.d41d8cd9.woff"
+  },
+  {
+    "revision": "45e1a01b2504d63ef84270ed864fdd03",
+    "url": "/static/media/geometria-bolditalic.45e1a01b.svg"
+  },
+  {
+    "revision": "d10227c4f0a3118bbe35a5385d174b17",
+    "url": "/static/media/geometria-bolditalic.d10227c4.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-bolditalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "da28738031c9abee782d2cfff81bcd5c",
+    "url": "/static/media/geometria-bolditalic.da287380.woff2"
+  },
+  {
+    "revision": "fbab91d0e316aca18ae959600db79efe",
+    "url": "/static/media/geometria-bolditalic.fbab91d0.ttf"
+  },
+  {
+    "revision": "052cd7b767fb368f8cc62c6635319a84",
+    "url": "/static/media/geometria-extrabold.052cd7b7.ttf"
+  },
+  {
+    "revision": "198ffb620e96f6b5085cb0561293f4a6",
+    "url": "/static/media/geometria-extrabold.198ffb62.svg"
+  },
+  {
+    "revision": "8b4d6db2dbd5ba3188dbc6381a39d825",
+    "url": "/static/media/geometria-extrabold.8b4d6db2.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-extrabold.d41d8cd9.woff"
+  },
+  {
+    "revision": "d7aec76617907d86efb4ebdd773bb30b",
+    "url": "/static/media/geometria-extrabold.d7aec766.woff2"
+  },
+  {
+    "revision": "416ef3a0ae0a40afa90df3afc9e3e68e",
+    "url": "/static/media/geometria-extrabolditalic.416ef3a0.ttf"
+  },
+  {
+    "revision": "b2ef294277c8aa8594acdce96813e40a",
+    "url": "/static/media/geometria-extrabolditalic.b2ef2942.woff2"
+  },
+  {
+    "revision": "c99b6bf08eb04c00d8caa26b59122154",
+    "url": "/static/media/geometria-extrabolditalic.c99b6bf0.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-extrabolditalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "e015803b8ba06bd1b732253329bbaaa7",
+    "url": "/static/media/geometria-extrabolditalic.e015803b.eot"
+  },
+  {
+    "revision": "494dfbff559e509784b6c41927f752ef",
+    "url": "/static/media/geometria-extralight.494dfbff.eot"
+  },
+  {
+    "revision": "bcea6398b661fc0070dcfda869329f61",
+    "url": "/static/media/geometria-extralight.bcea6398.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-extralight.d41d8cd9.woff"
+  },
+  {
+    "revision": "ebb3ff9594c5b1f9df7b7ff03989a1ec",
+    "url": "/static/media/geometria-extralight.ebb3ff95.woff2"
+  },
+  {
+    "revision": "f315466bd79c9d26c821162197ba1d67",
+    "url": "/static/media/geometria-extralight.f315466b.svg"
+  },
+  {
+    "revision": "5ccf27d9b33c0ab8e14273655ca00af8",
+    "url": "/static/media/geometria-extralightitalic.5ccf27d9.svg"
+  },
+  {
+    "revision": "7805f244214fa9cf58df0a43f2b93a55",
+    "url": "/static/media/geometria-extralightitalic.7805f244.ttf"
+  },
+  {
+    "revision": "971d0670a0bb9f0baca80bef2173f69a",
+    "url": "/static/media/geometria-extralightitalic.971d0670.woff2"
+  },
+  {
+    "revision": "bcf340f433f1210418a9d2e72d784628",
+    "url": "/static/media/geometria-extralightitalic.bcf340f4.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-extralightitalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "6b3c88ff9a634a5e1a4f193112157f7a",
+    "url": "/static/media/geometria-heavy.6b3c88ff.eot"
+  },
+  {
+    "revision": "7ed1493ebb9b3f936f025ce4285544a5",
+    "url": "/static/media/geometria-heavy.7ed1493e.ttf"
+  },
+  {
+    "revision": "7f82ae3c44db26ac9ff26b5d8c5dc909",
+    "url": "/static/media/geometria-heavy.7f82ae3c.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-heavy.d41d8cd9.woff"
+  },
+  {
+    "revision": "f04054f8ebcc64a1038f729738eed54d",
+    "url": "/static/media/geometria-heavy.f04054f8.svg"
+  },
+  {
+    "revision": "2ba16ae3a276d5c21e6c6bd8ef7af9f9",
+    "url": "/static/media/geometria-heavyitalic.2ba16ae3.ttf"
+  },
+  {
+    "revision": "6384550b809fe782843c342f87c3a5cf",
+    "url": "/static/media/geometria-heavyitalic.6384550b.woff2"
+  },
+  {
+    "revision": "8491694afc369914a31dec3b7f0ddc6d",
+    "url": "/static/media/geometria-heavyitalic.8491694a.svg"
+  },
+  {
+    "revision": "890e0c6eada28298313ef8088a5e85b3",
+    "url": "/static/media/geometria-heavyitalic.890e0c6e.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-heavyitalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "0ee195094ae4c0f0441498dae2019661",
+    "url": "/static/media/geometria-italic.0ee19509.svg"
+  },
+  {
+    "revision": "920970cd7d8eb3671c26a5eee061903e",
+    "url": "/static/media/geometria-italic.920970cd.ttf"
+  },
+  {
+    "revision": "a09caa037bb876d250a8ff67f7b9370d",
+    "url": "/static/media/geometria-italic.a09caa03.eot"
+  },
+  {
+    "revision": "a3e64dc6b502d0089ac17845c83067c1",
+    "url": "/static/media/geometria-italic.a3e64dc6.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-italic.d41d8cd9.woff"
+  },
+  {
+    "revision": "4d6a57fdab429e111b3da042aa63ef2f",
+    "url": "/static/media/geometria-light.4d6a57fd.eot"
+  },
+  {
+    "revision": "561260acbfbf91161f039f9b1b76e2ce",
+    "url": "/static/media/geometria-light.561260ac.svg"
+  },
+  {
+    "revision": "90ad92ba651bc1af1ce286fb2933da2d",
+    "url": "/static/media/geometria-light.90ad92ba.ttf"
+  },
+  {
+    "revision": "aa1ccb08e3a07119060a34cc473afcf4",
+    "url": "/static/media/geometria-light.aa1ccb08.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-light.d41d8cd9.woff"
+  },
+  {
+    "revision": "00369be9f136998e4879c0cbeabd885d",
+    "url": "/static/media/geometria-lightitalic.00369be9.eot"
+  },
+  {
+    "revision": "59c0beae7f4acad380eae99138b4d507",
+    "url": "/static/media/geometria-lightitalic.59c0beae.woff2"
+  },
+  {
+    "revision": "7f1fd2ec4931b1c68e756832a18fc257",
+    "url": "/static/media/geometria-lightitalic.7f1fd2ec.ttf"
+  },
+  {
+    "revision": "9760879beb8308999666f4849aa91cdd",
+    "url": "/static/media/geometria-lightitalic.9760879b.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-lightitalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "74c8b26b6dc72c695191d912a734747d",
+    "url": "/static/media/geometria-medium.74c8b26b.woff2"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/static/media/geometria-medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "90152a4f5c76144f11d47dcf43ae12d9",
+    "url": "/static/media/geometria-medium.90152a4f.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-medium.d41d8cd9.woff"
+  },
+  {
+    "revision": "e5d8fe3c6c7298ce9ad8038dc0773f6f",
+    "url": "/static/media/geometria-medium.e5d8fe3c.svg"
+  },
+  {
+    "revision": "2fe1e3d646b9daa90dcef43605a3b3d4",
+    "url": "/static/media/geometria-mediumitalic.2fe1e3d6.svg"
+  },
+  {
+    "revision": "4f5d0cff4b01ea43b37a8d35675da88b",
+    "url": "/static/media/geometria-mediumitalic.4f5d0cff.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-mediumitalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "e43e1dfc5da63e41b0cc2fd0f8bcd3c7",
+    "url": "/static/media/geometria-mediumitalic.e43e1dfc.woff2"
+  },
+  {
+    "revision": "f53e6bc54776e97c6fae19447f66fffd",
+    "url": "/static/media/geometria-mediumitalic.f53e6bc5.ttf"
+  },
+  {
+    "revision": "3443cb5d2a4af293858f17ce59d00e34",
+    "url": "/static/media/geometria-thin.3443cb5d.woff2"
+  },
+  {
+    "revision": "3c54068a975e0827d3d50bc15e9f52e4",
+    "url": "/static/media/geometria-thin.3c54068a.svg"
+  },
+  {
+    "revision": "4ac4107ad6d9bb9a480240e74b1484fe",
+    "url": "/static/media/geometria-thin.4ac4107a.eot"
+  },
+  {
+    "revision": "b1fcd3ff3386794c14b389ffdaf9b3d6",
+    "url": "/static/media/geometria-thin.b1fcd3ff.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-thin.d41d8cd9.woff"
+  },
+  {
+    "revision": "0afce159d317123e7b660d23d8ff8a11",
+    "url": "/static/media/geometria-thinitalic.0afce159.eot"
+  },
+  {
+    "revision": "2d9a01e10353effbe0183fa7215601de",
+    "url": "/static/media/geometria-thinitalic.2d9a01e1.woff2"
+  },
+  {
+    "revision": "47d3ace5d9ffb7edb7a04065791dfb93",
+    "url": "/static/media/geometria-thinitalic.47d3ace5.ttf"
+  },
+  {
+    "revision": "a2ee29ead21adf9cb316213b54a37671",
+    "url": "/static/media/geometria-thinitalic.a2ee29ea.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria-thinitalic.d41d8cd9.woff"
+  },
+  {
+    "revision": "148c8de5355aa2e2be4e11a9652a38b6",
+    "url": "/static/media/geometria.148c8de5.svg"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/static/media/geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "778525f6405ea0daa98d885a4ba17189",
+    "url": "/static/media/geometria.778525f6.eot"
+  },
+  {
+    "revision": "79b74902abd4a44f8314eb23e81d5231",
+    "url": "/static/media/geometria.79b74902.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/geometria.d41d8cd9.woff"
+  }
+]);
